9a4771ec9107f864b6a814696cd15d4091225d7d
lahcen404 <lahcen.maskour2003@gmail.com>
2025-08-07 11:34:33 +0100

    create Author Entity

--- livre/src/main/java/com/challenge/livre/models/Author.java
+++ livre/src/main/java/com/challenge/livre/models/Author.java
@@ -1,4 +1,41 @@
 package com.challenge.livre.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String biography;
+
+
+    // getters & setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
